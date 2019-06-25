f70215cd64983d2681fd18c0f5ed7393b0eb1228
moolisi <moolisi@qq.com>
2019-06-25 23:53:50 +0800

    add

--- src/components/panel/panel.vue
+++ src/components/panel/panel.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="panel">
-    <header class="panel-header border-b" :class="panelHeaderStyle" v-text="title"></header>
+    <header class="panel-header border-b" :class="panelHeaderStyle">
+      <span class="name" v-text="`- ${title} -`"></span>
+      <router-link v-if="right.name" class="right" :to="right.path" v-text="right.name"></router-link>
+    </header>
     <slot class="panel-body"></slot>
   </div>
 </template>
@@ -20,19 +23,37 @@
         default() {
           return '标题'
         }
+      },
+      right: {
+        type: Object,
+        default() {
+          return {}
+        }
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-/*@import "../../assets/css/scss/_border.scss";*/
+@import "../../assets/css/rem";
 .panel {
   width: 100%;
   background: #ffffff;
-  .header {
-    height: rem(40);
-    line-height: rem(40);
+  .panel-header {
+    position: relative;
+    height: rem(100);
+    line-height: rem(100);
+    text-align: center;
+    font-size: rem(36);
+    .right {
+      position: absolute;
+      top: 50%;
+      right: rem(10);
+      transform: translateY(-50%);
+      color: #999999;
+      font-size: rem(24);
+      text-decoration: none;
+    }
   }
   .panel-body {
     padding: 20px;
--- src/views/home/components/green-hand.vue
+++ src/views/home/components/green-hand.vue
@@ -1,5 +1,5 @@
 <template>
-  <panel class="mt20" :title="'新手特权'">asdfasd</panel>
+  <panel class="mt20" :title="'新手特权'" :right="{name: '查看更多任务', path: '/'}">asdfasd</panel>
 </template>
 
 <script>
